9e0046eaa1b9c23276a2ca6256a803eaf13b63fd
CharlesDexterW <agarces2381@gmail.com>
2023-03-04 00:58:06 -0500

    added normal curve plot code. error on Y = stats

--- Lineal_Regression_Model.R
+++ Lineal_Regression_Model.R
@@ -1,8 +1,10 @@
 # Read in the dataset
-data <- read.csv("dataset.csv")
+library(readxl)
 
+data <- read_excel("Salc_Students_Data.xlsx")
+str(data)
 # Fit the linear regression model
-model <- lm(Test_Score ~ Study_Hours, data = data)
+model <- lm(Test_Score ~ Total_Study_Hours, data = data)
 
 # Print the model summary
 summary(model)
@@ -11,14 +13,22 @@ summary(model)
 # Load the ggplot2 library
 library(ggplot2)
 
-# Read in the dataset
-data <- read.csv("dataset.csv")
-
 # Fit the linear regression model
-model <- lm(Test_Score ~ Study_Hours, data = data)
+model <- lm(Test_Score ~ Total_Study_Hours, data = data)
 
 # Create a scatter plot with regression line
-ggplot(data, aes(x = Study_Hours, y = Test_Score)) +
+ggplot(data, aes(x = Total_Study_Hours, y = Test_Score)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
-  labs(x = "Study Hours", y = "Test Score", title = "Study Hours vs. Test Score")
+  labs(x = "Study_Hours", y = "Test Score", title = "Study Hours vs. Test Score")
+
+# Calculate mean and standard deviation of Test_Score
+mean_score <- mean(data$Test_Score)
+sd_score <- sd(data$Test_Score)
+
+# Create a histogram with normal distribution curve
+ggplot(data, aes(x = Test_Score, y = after_stat(density))) +
+  geom_histogram(color = "black", fill = "white", bins = 20) +
+  stat_function(fun = dnorm, args = list(mean = mean_score, sd = sd_score), color = "red", linewidth = 1) +
+  labs(x = "Test Score", y = "Density", title = "Distribution of Test Scores")
+
--- Multivariate_regression_test_(teacher_evaluation).R
+++ Multivariate_regression_test_(teacher_evaluation).R
@@ -7,7 +7,6 @@ data <- data.frame(studying_hours, teacher_interventions, test_scores)
 
 # Fit multiple regression model
 model <- lm(test_scores ~ studying_hours + teacher_interventions, data = data)
-
 # View model summary
 summary(model)
 
@@ -22,4 +21,7 @@ summary(model)
   #studying_hours and teacher_interventions. We store this model object in the 
   #variable model.
 
-#Finally, we use the summary function to view a summary of the model's coefficients, including their estimates, standard errors, t-values, and p-values. This output can help us understand the strength and direction of the relationships between the variables, as well as their statistical significance.
+#Finally, we use the summary function to view a summary of the model's coefficients, 
+#including their estimates, standard errors, t-values, and p-values. This output can 
+#help us understand the strength and direction of the relationships between the variables, 
+#as well as their statistical significance.
